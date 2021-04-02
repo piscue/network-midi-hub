ebd89388ca10bf2c9f81d9b292a8dbf6eff428d2
piscue <piscue@gmail.com>
2021-04-02 23:34:46 +0200

    Add midi thru to output

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import mido
+import argparse
 
-
-def input_midi_port():
-    with mido.open_input('vmidi input', virtual=True) as inport:
-        for msg in inport:
-            print(msg)
+def get_args():
+    parser = argparse.ArgumentParser(description='Get vars')
+    parser.add_argument('--thru', '-t', action='store_true')
+    parser.parse_args()
+    return parser.parse_known_args()
 
 
 def main():
-    input_midi_port()
+    vars, unknown_vars = get_args()
+    input_midi = mido.open_input('vmidi input', virtual=True)
+    output_midi = mido.open_output('vmidi output', virtual=True)
+    messages_to_output = []
+    while True:
+        msg = input_midi.poll()
+        for msg in input_midi.poll():
+            if vars.thru:
+                messages_to_output.append(msg)
+        if len(messages_to_output) > 0:
+            for message in messages_to_output:
+                output_midi.send(message)
+            messages_to_output = []
+
 
 if __name__ == "__main__":
     main()
