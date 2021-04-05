617be0c210e802593707214cc97e6e626f8f928d
piscue <piscue@gmail.com>
2021-04-05 16:13:01 +0200

    Multiple client midi-hub, mix minus

--- server.py
+++ server.py
@@ -17,31 +17,56 @@ def get_args():
     return parser.parse_args()
 
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
+def accept_wrapper(sock, sockets):
+    conn, addr = sock.accept()
     print("accepted connection from", addr)
     conn.setblocking(False)
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     sel.register(conn, events, data=data)
+    sockets.append(conn)
+    print(sockets)
+    return sockets
 
 
-def service_connection(key, mask):
+
+def service_connection(key, mask, sockets):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print("closing connection to", data.addr)
-            sel.unregister(sock)
-            sock.close()
+        try:
+            recv_data = sock.recv(1024)  # Should be ready to read
+            if recv_data:
+                # data.outb += recv_data
+                # not acumulating data, instead of appending
+                data.outb = recv_data
+            else:
+                print("closing connection to", data.addr)
+                sel.unregister(sock)
+                # print(sock)
+                # print(sockets)
+                sock.close()
+                if sock in sockets:
+                    sockets.remove(sock)
+        except ConnectionResetError as e:
+            print(e)
     if mask & selectors.EVENT_WRITE:
         if data.outb:
-            print("echoing", repr(data.outb), "to", data.addr)
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+            print(data.outb)
+            for socks in sockets:
+                if socks != sock:
+                    data_socks = data.outb
+                    try:
+                        sent = socks.send(data_socks)
+                    except BrokenPipeError as e:
+                        print(e)
+            try:
+                data.outb = data.outb[sent:]
+            except UnboundLocalError as e:
+                # print('failed to mark as sent, probably only one client')
+                # print(e)
+                pass
+    return sockets
 
 
 def main():
@@ -53,19 +78,23 @@ def main():
     print("listening on", (host, port))
     lsock.setblocking(False)
     sel.register(lsock, selectors.EVENT_READ, data=None)
+    sockets = []
 
     try:
         while True:
             events = sel.select(timeout=None)
             for key, mask in events:
                 if key.data is None:
-                    accept_wrapper(key.fileobj)
+                    sockets = accept_wrapper(key.fileobj, sockets)
                 else:
-                    service_connection(key, mask)
+                    sockets = service_connection(key, mask, sockets)
             time.sleep(0.002)
     except KeyboardInterrupt:
         print("keyboard interrupt, exiting")
     finally:
+        for sock in sockets:
+            sock.close()
+        lsock.close()
         sel.close()
 
 
